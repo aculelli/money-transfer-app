ac91066bfec31cd3b8b59e47e7660839ef2eaa8b
Alice Culin-Ellison <aculelli@gmail.com>
2021-05-03 15:51:11 -0400

    First Commit

---
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransfersController.java
@@ -0,0 +1,43 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.TransfersDAO;
+import com.techelevator.tenmo.model.Transfers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@PreAuthorize("isAuthenticated()")
+@RestController
+public class TransfersController {
+
+    @Autowired
+    private TransfersDAO transfersDAO;
+
+    @RequestMapping(path = "/transfers", method = RequestMethod.GET)
+    public List<Transfers> listAllTransfers() {
+        return transfersDAO.listAllTransfers();
+    }
+
+    @RequestMapping(path = "/transfers/{userAccountId}", method = RequestMethod.GET)
+    public List<Transfers> listAllTransfersForUser(@PathVariable ("userAccountId") Long currentUserAccountId){
+        return transfersDAO.listAllTransfersForUser(currentUserAccountId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "accounts/{userId}/transfer/{receiverId}", method = RequestMethod.POST)
+    public void addTransfer(@RequestParam String transferType, @RequestParam String transferStatus,
+                            @PathVariable("userId") Long currentUserId,
+                            @PathVariable("receiverId") Long receiverUserId,
+                            @RequestParam BigDecimal amountToTransfer){
+        transfersDAO.addTransfer(transferType, transferStatus, currentUserId, receiverUserId, amountToTransfer);
+    }
+
+
+
+
+
+}
